7de87c0c0c040e0434b90c61f7a8b3f7be1ee659
Anthony Brunasso <AnthonyBrunasso@gmail.com>
2017-05-08 19:39:15 -0700

    Invoke SDL functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,41 @@
 #include <windows.h>
 
 INT WinMain(HINSTANCE, HINSTANCE, PSTR, INT) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
-    SDL_Quit();
+
+    SDL_Window* window = SDL_CreateWindow("Learn Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 720, 480, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event e;
+    while (true) {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_QUIT:
+                return 0;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
 
     return 0;
 }
