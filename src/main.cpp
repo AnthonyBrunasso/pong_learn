a6021fef8e9cc276df2517d1cb0ce5c87f981cc8
Anthony Brunasso <AnthonyBrunasso@gmail.com>
2017-05-08 19:02:40 -0700

    Added sdl.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,13 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <SDL.h>
+#include <windows.h>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+INT WinMain(HINSTANCE, HINSTANCE, PSTR, INT) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
     }
+    SDL_Quit();
 
     return 0;
 }
