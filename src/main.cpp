59529ec410d6b2d8549a2bfb522e6e0ff6c573b1
Anthony Brunasso <AnthonyBrunasso@gmail.com>
2017-05-09 20:32:51 -0700

    More stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,14 @@
 static const int WINDOW_WIDTH = 720;
 static const int WINDOW_HEIGHT = 480;
 static const float PADDLE_SPEED = 4.0f;
+static const int PADDLE_WIDTH = 30;
+static const int PADDLE_HEIGHT = 70;
+// Dimension of bouncing square.
+static const int PONG_WIDTH = 15;
+static const int PONG_HEIGHT = 15;
+static const float PONG_SPEED = 2.0f;
 
-
-void update_simulation(SDL_Rect& p1, SDL_Rect& p2, double dt) {
+void update_simulation(SDL_Rect& p1, SDL_Rect& p2, SDL_Rect& pong, float& xv, float& yv, double dt) {
     const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
     if (key_state[SDL_SCANCODE_W] && p1.y > 0) {
@@ -27,6 +32,26 @@ void update_simulation(SDL_Rect& p1, SDL_Rect& p2, double dt) {
     if (key_state[SDL_SCANCODE_DOWN] && p2.y < WINDOW_HEIGHT - p2.h) {
         p2.y += dt * PADDLE_SPEED;
     }
+
+    static int x_valid = pong.x;
+    static int y_valid = pong.y;
+
+    if (SDL_HasIntersection(&p1, &pong) || SDL_HasIntersection(&p2, &pong)) {
+        xv *= -1;
+        yv *= -1;
+
+        pong.x = x_valid;
+        pong.y = y_valid;
+    }
+    else {
+        x_valid = pong.x;
+        y_valid = pong.y;
+    }
+
+    std::cout << dt * xv << std::endl;
+
+    pong.x += dt * xv;
+    pong.y += dt * yv;
 }
 
 int main(int argc, char* argv[]) {
@@ -53,8 +78,10 @@ int main(int argc, char* argv[]) {
     double t = 0.0;
     auto current = std::chrono::system_clock::now().time_since_epoch();
 
-    SDL_Rect p1; p1.x = 0; p1.y = WINDOW_HEIGHT / 2 - 70; p1.w = 30; p1.h = 70;
-    SDL_Rect p2; p2.x = WINDOW_WIDTH - 30; p2.y = WINDOW_HEIGHT / 2 - 70; p2.w = 30; p2.h = 70;
+    SDL_Rect p1; p1.x = 0; p1.y = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT; p1.w = PADDLE_WIDTH; p1.h = PADDLE_HEIGHT;
+    SDL_Rect p2; p2.x = WINDOW_WIDTH - PADDLE_WIDTH; p2.y = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT; p2.w = PADDLE_WIDTH; p2.h = PADDLE_HEIGHT;
+    SDL_Rect pong; pong.x = WINDOW_WIDTH / 2; pong.y = WINDOW_HEIGHT / 2 - PONG_HEIGHT; pong.w = PONG_WIDTH; pong.h = PONG_HEIGHT;
+    float xv = PONG_SPEED, yv = 0.0;
 
     SDL_Event e;
     while (true) {
@@ -76,7 +103,7 @@ int main(int argc, char* argv[]) {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        update_simulation(p1, p2, frame_time);
+        update_simulation(p1, p2, pong, xv, yv, frame_time);
         t += frame_time;
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
@@ -84,6 +111,9 @@ int main(int argc, char* argv[]) {
 
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &p2);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &pong);
 
         SDL_RenderPresent(renderer);
     }
